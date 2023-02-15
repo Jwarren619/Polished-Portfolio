3f045cf9b596bb7740a0b5e5af2d2741ba631d60
Jwarren619 <jjeremiahwarren@yahoo.com>
2023-02-14 23:48:48 -0600

    adding coding to my react profile

---
+++ my-app/src/components/Profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Profile() {
+
+    return ( 
+        <div className="container">
+      <h1>My Skillsets</h1>
+      
+      <ul className="parent">
+        <li>Node.js</li>
+        <li>JavaScript</li>
+        <li>Front End Development</li>
+        <li>SQL</li>
+      </ul>
+    </div>
+    )
+}
